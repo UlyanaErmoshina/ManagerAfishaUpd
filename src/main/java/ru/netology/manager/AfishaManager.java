2c269425ed6c1a69db773b5eaac27da98e44c6f8
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-06-30 21:12:18 +0300

    Update commit

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -4,61 +4,68 @@ import ru.netology.domain.Movie;
 
 public class AfishaManager {
 
-  private Movie[] movies = new Movie[0];
-  private int CustomQuantityMovies;
-  private static int DefaultQuantityMovies = 10;
+    private Movie[] movies = new Movie[0];
+    private int CustomQuantityMovies;
+    private static int DefaultQuantityMovies = 10;
 
-  public AfishaManager(int customQuantityMovies) {
+    public AfishaManager(int customQuantityMovies) {
 
-    CustomQuantityMovies = customQuantityMovies;
-  }
-
-  public AfishaManager() {
-  }
-
-
-  public void add(Movie item) {
-    int length = movies.length + 1;
-    Movie[] tmp = new Movie[length];
-    System.arraycopy(movies, 0, tmp, 0, movies.length);
-    int lastIndex = tmp.length - 1;
-    tmp[lastIndex] = item;
-    movies = tmp;
-  }
-
-
-
-  public Movie[] getAll() {
-    int lengths;
-
-    lengths = Math.min (Math.min(movies.length, CustomQuantityMovies), DefaultQuantityMovies);
-
-//    if (movies.length<CustomQuantityMovies)
-//      lengths= movies.length;
-//    else
-//    if (CustomQuantityMovies < DefaultQuantityMovies)
-//      lengths = CustomQuantityMovies;
-//    else
-//      lengths = DefaultQuantityMovies;
-    Movie[] result = new Movie[lengths];
-    for (int i = 0; i < lengths; i++) {
-      int index = movies.length - i - 1;
-      result[i] = movies[index];
-    }
-    return result;
-  }
-
-  public void removeById(int id) {
-    int length = movies.length - 1;
-    Movie[] tmp = new Movie[length];
-    int index = 0;
-    for (Movie item : movies) {
-      if (item.getId() != id) {
-        tmp[index] = item;
-        index++;
-      }
+        CustomQuantityMovies = customQuantityMovies;
     }
 
-    movies = tmp;
-  }
+    public AfishaManager() {
+        CustomQuantityMovies = 0;
+    }
+
+
+    public void add(Movie item) {
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        movies = tmp;
+    }
+
+
+    public Movie[] getAll() {
+        int lengths;
+
+//    lengths = Math.min (Math.min(movies.length, CustomQuantityMovies), DefaultQuantityMovies);
+        if (CustomQuantityMovies == 0)
+            if (movies.length < DefaultQuantityMovies)
+                lengths = movies.length;
+            else
+                lengths = DefaultQuantityMovies;
+        else
+            lengths = Math.min(Math.min(movies.length, CustomQuantityMovies), DefaultQuantityMovies);
+
+
+//        if (movies.length < CustomQuantityMovies && CustomQuantityMovies != 0)
+//            lengths = movies.length;
+//        else if (CustomQuantityMovies < DefaultQuantityMovies)
+//            lengths = CustomQuantityMovies;
+//        else
+//            lengths = DefaultQuantityMovies;
+        Movie[] result = new Movie[lengths];
+        for (int i = 0; i < lengths; i++) {
+            int index = movies.length - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+
+    public void removeById(int id) {
+        int length = movies.length - 1;
+        Movie[] tmp = new Movie[length];
+        int index = 0;
+        for (Movie item : movies) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+
+        movies = tmp;
+    }
 }
