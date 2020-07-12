83541396bd4d48210c3cdf1c10d12691128ed2f4
andrew-pahomov <andrew-pahomov@yandex.ru>
2020-07-12 12:35:53 +0300

    fix project

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -4,43 +4,30 @@ import ru.netology.domain.Movie;
 import ru.netology.repository.AfishaRepository;
 
 public class AfishaManager {
-  private Movie[] movies = new Movie[0];
-  private int itemsToShow = defaultQuantityMovies;
-  private static int defaultQuantityMovies = 10;
+    private int defaultQuantityMovies = 10;
+    private AfishaRepository repository;
 
-  private AfishaRepository repository;
-
-  public AfishaManager(int customQuantityMovies) {
-    if (customQuantityMovies > 0) {
-
-      itemsToShow = customQuantityMovies;
+    public AfishaManager() {
     }
-    else
-      itemsToShow = defaultQuantityMovies;
-  }
 
-  public AfishaManager(AfishaRepository repository) {
-    this.repository = repository;
-  }
-
-
-  public void add(Movie item) {
-    repository.save(item);
-  }
-
-  public Movie[] getAll() {
-    Movie[] items = repository.findAll();
-    int lengths;
-
-    lengths = Math.min (items.length, itemsToShow);
-
-
-    Movie[] result = new Movie[lengths];
-    for (int i = 0; i < lengths; i++) {
-      int index = items.length - i - 1;
-      result[i] = items[index];
+    public AfishaManager(int defaultQuantityMovies, AfishaRepository repository) {
+        this.defaultQuantityMovies = defaultQuantityMovies;
+        this.repository = repository;
     }
-    return result;
-  }
+
+    public void add(Movie item) {
+        repository.save(item);
+    }
+
+    public Movie[] getAll() {
+        Movie[] items = repository.findAll();
+        int lengths = Math.min(items.length, defaultQuantityMovies);
+        Movie[] result = new Movie[lengths];
+        for (int i = 0; i < lengths; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
 
 }
